5e33b5f290f9aaf87b401b01d8786e3ac9e65c9d
douglasro <douglasro@74311-U.asav.brm>
2019-12-05 17:08:06 -0200

    com ramificaçao de código

--- src/console/Console.java
+++ src/console/Console.java
@@ -16,6 +16,14 @@ public class Console {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int contador = 0;
+        while(contador < 5){
+            System.out.println(contador);
+        }
+        if (contador == 5){
+            System.out.println("Contador igual a 5");
+        }
+        
     }
     
 }
